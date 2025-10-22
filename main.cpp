7cf0d6a66eaadbba8509d91ea3878b2320575f34
jdcccc <1704690357@qq.com>
2025-10-23 02:31:20 +0800

    texture success

--- main.cpp
+++ main.cpp
@@ -44,6 +44,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // glfw 窗口创建
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
